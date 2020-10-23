1fdaba9b0b69063231091b88d56824ef54a124b0
Robert <Robert.wang0804@gmail.com>
2020-10-23 16:34:01 +0200

    new start

--- leetcode/part1.py
+++ leetcode/part1.py
@@ -1114,4 +1114,13 @@ class Solution:
 
         return dp[0][0]
 
+#202
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        while n not in seen:
+            seen.add(n)
+            n = sum([int(x) **2 for x in str(n)])
+        return n == 1
 
+#204
